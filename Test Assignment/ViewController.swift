c8779a50f6980354dbc008d880d18c44b71122aa
Ahmed Moeen Serdah <aserdah@gmail.com>
2022-06-24 09:27:46 +0300

    Initial Commit

---
+++ Test Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test Assignment
+//
+//  Created by Ahmed Serdah on 24/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
